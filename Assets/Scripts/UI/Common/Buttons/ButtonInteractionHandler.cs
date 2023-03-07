5e88242a6d8e18ec0e23a86fcca9a53b79f3facf
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-03-07 18:16:49 +0200

    Basic ui interaction scripts added

---
+++ Assets/Scripts/UI/Common/Buttons/ButtonInteractionHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public abstract class ButtonInteractionHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Button _button;
+
+    public Button ButtonComponent { get => _button; private set => _button = value; }
+
+    private void Awake()
+    {
+        if (TryGetComponent(out Button button))
+        {
+            ButtonComponent = button;
+            ButtonComponent.onClick.AddListener(ButtonActivated);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ProcessEvent_PointerEnter(eventData);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ProcessEvent_PointerExit(eventData);
+    }
+
+    public void SetButtonActive()
+    {
+        ButtonComponent.interactable = true;
+    }
+
+    public void SetButtonDisabled()
+    {
+        ButtonComponent.interactable = false;
+    }
+
+    public abstract void ProcessEvent_PointerEnter(PointerEventData eventData);
+
+    public abstract void ProcessEvent_PointerExit(PointerEventData eventData);
+
+    public abstract void ButtonActivated();
+}
---
+++ Assets/Scripts/UI/Common/Canvas/PanelActivationManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelActivationManager : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void ShowPanel()
+    {
+        SetCanvasActivationState(true);
+    }
+
+    public void HidePanel()
+    {
+        SetCanvasActivationState(false);
+    }
+
+    public void SetCanvasActivationState(bool isActive)
+    {
+        if (isActive)
+        {
+            canvasGroup.alpha = 1f;
+        }
+        else
+        {
+            canvasGroup.alpha = 0f;
+        }
+
+        canvasGroup.interactable = isActive;
+        canvasGroup.blocksRaycasts = isActive;
+    }
+}
