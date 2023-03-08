7ab171f1699abe177cb96f3e341765bcfbc7d34d
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-03-08 16:38:24 +0200

    Input manager added

--- Assets/Resources/ControllersInstaller.cs
+++ Assets/Resources/ControllersInstaller.cs
@@ -6,9 +6,11 @@ public class ControllersInstaller : MonoInstaller
     [Header("References")]
     [Space]
     [SerializeField] private AudioManager audioManager;
+    [SerializeField] private InputManager inputManager;
 
     public override void InstallBindings()
     {
         Container.Bind<AudioManager>().FromInstance(audioManager).AsSingle().NonLazy();
+        Container.Bind<InputManager>().FromInstance(inputManager).AsSingle().NonLazy();
     }
 }
