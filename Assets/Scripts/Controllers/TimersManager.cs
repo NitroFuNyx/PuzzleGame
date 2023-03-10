b2105d564e6726e38e820734ce12c0a42a6bcb06
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-03-10 13:04:06 +0200

    Timers Manager added

---
+++ Assets/Scripts/Controllers/TimersManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimersManager : MonoBehaviour
+{
+    public void StartTimer(float startTimerValue)
+    {
+
+    }
+}
