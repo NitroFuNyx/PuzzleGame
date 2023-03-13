f5da08639cd60e5a72537437ceaad30249386646
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-03-13 13:08:44 +0200

    Scene Updated

--- Assets/Scripts/Save Load System/SaveLoadSystem.cs
+++ Assets/Scripts/Save Load System/SaveLoadSystem.cs
@@ -5,12 +5,13 @@ using System.Runtime.Serialization.Formatters.Binary;
 public static class SaveLoadSystem
 {
     private const string PlayerDataSaveFilePath = "Assets/Save Files/PlayerMainDataSave.bsf";
+    private static string playerDataSaveFilePathInBuild = $"{Application.persistentDataPath}/PlayerMainDataSave.bsf";
 
     public static void SavePlayerData(PlayerDataManager playerDataManager)
     {
         BinaryFormatter formatter = new BinaryFormatter();
  
-        FileStream stream = new FileStream(PlayerDataSaveFilePath, FileMode.Create);
+        FileStream stream = new FileStream(GetSaveFilePath(), FileMode.Create);
 
         PlayerMainData playerMainData = new PlayerMainData(playerDataManager);
 
@@ -22,19 +23,30 @@ public static class SaveLoadSystem
     {
         PlayerMainData data = null;
 
-        if (File.Exists(PlayerDataSaveFilePath))
+        if (File.Exists(GetSaveFilePath()))
         {
             BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(PlayerDataSaveFilePath, FileMode.Open);
+            FileStream stream = new FileStream(GetSaveFilePath(), FileMode.Open);
 
             data = formatter.Deserialize(stream) as PlayerMainData;
             stream.Close();
         }
         else
         {
-            Debug.Log($"No Save File {PlayerDataSaveFilePath}");
+            Debug.Log($"No Save File {GetSaveFilePath()}");
         }
 
          return data;
+    }
+
+    private static string GetSaveFilePath()
+    {
+        string path = playerDataSaveFilePathInBuild;
+
+#if UNITY_EDITOR
+        path = PlayerDataSaveFilePath;
+#endif
+
+        return path;
     }
 }
