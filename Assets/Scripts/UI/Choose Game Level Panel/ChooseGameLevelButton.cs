6326ef65e4c763c3cf3489a734935705757b0320
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-03-16 14:09:32 +0200

    Locked level button interaction updated

--- Assets/Scripts/UI/Choose Game Level Panel/ChooseGameLevelButton.cs
+++ Assets/Scripts/UI/Choose Game Level Panel/ChooseGameLevelButton.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
+using DG.Tweening;
+using TMPro;
 
 public class ChooseGameLevelButton : ButtonInteractionHandler
 {
@@ -8,9 +11,26 @@ public class ChooseGameLevelButton : ButtonInteractionHandler
     [SerializeField] private GameLevelTypes gameType;
     [SerializeField] private GameLevelStates levelState;
     [SerializeField] private int levelIndex;
+    [Header("Internal References")]
+    [Space]
+    [SerializeField] private Image lockImage;
+    [SerializeField] private TextMeshProUGUI costAmountText;
+    [Header("Lock Rotation Data")]
+    [Space]
+    [SerializeField] private Vector3 rotationPunchVector = new Vector3(1f, 1f, 1f);
+    [SerializeField] private float punchDuration = 1f;
+    [SerializeField] private int punchFreequency = 3;
+    [Header("Cost Text Data")]
+    [Space]
+    [SerializeField] private Vector3 costTextPunchVector = new Vector3(0.2f, 0.2f, 0.2f);
+    [SerializeField] private int costTextPunchScaleFreequency = 4;
+    [SerializeField] private Color costTextBlockedColor;
+    
 
     private MainUI _mainUI;
     private CurrentGameManager _currentGameManager;
+
+    private bool isAnimationInProcess = false;
 
     #region Zenject
     [Inject]
@@ -28,6 +48,27 @@ public class ChooseGameLevelButton : ButtonInteractionHandler
             ShowAnimation_ButtonPressed();
             _currentGameManager.ActivateGameLevelEnvironment(levelIndex);
             StartCoroutine(ActivateDelayedButtonMethodCoroutine(_mainUI.ShowGameLevelUI));
+        }
+        else
+        {
+            if(!isAnimationInProcess)
+            {
+                isAnimationInProcess = true;
+                lockImage.transform.DOKill();
+                costAmountText.DOKill();
+                costAmountText.transform.DOKill();
+                costAmountText.DOColor(costTextBlockedColor, punchDuration / 2).OnComplete(() =>
+                {
+                    costAmountText.transform.DOScale(Vector3.one, punchDuration / 2);
+                    costAmountText.DOColor(Color.white, punchDuration);
+                });
+                costAmountText.transform.DOPunchScale(costTextPunchVector, punchDuration, costTextPunchScaleFreequency);
+                lockImage.transform.DOPunchRotation(rotationPunchVector, punchDuration, punchFreequency).OnComplete(() =>
+                {
+                    lockImage.transform.DORotate(Vector3.zero, punchDuration);
+                    isAnimationInProcess = false;
+                });
+            }
         }
     }
 
