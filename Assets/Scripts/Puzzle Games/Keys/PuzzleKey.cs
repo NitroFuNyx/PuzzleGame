1ff9a8d01ceaf3a02abeef5e7410d8474698042d
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-03-29 10:32:53 +0300

    Puzzle Key Image Moved To Inventory vfx added

--- Assets/Scripts/Puzzle Games/Keys/PuzzleKey.cs
+++ Assets/Scripts/Puzzle Games/Keys/PuzzleKey.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using Zenject;
 
 public class PuzzleKey : PuzzleGameItemInteractionHandler
 {
     [Header("Key Data")]
     [Space]
     [SerializeField] private int keyIndex = 0;
+    [Header("Rotate Data")]
+    [Space]
+    [SerializeField] private Vector3 rotationPunchVector = new Vector3(1f, 1f, 1f);
+    [SerializeField] private float punchDuration = 1f;
+    [SerializeField] private int punchFreequency = 3;
+    [Header("Jump Data")]
+    [Space]
+    [SerializeField] private float jumpDelta = 1f;
+    [SerializeField] private float jumpPower = 1f;
+    [SerializeField] private float jumpDuration = 1f;
+
+    private PuzzleGameUI _puzzleGameUI;
+
+    private SpriteRenderer spriteRenderer;
 
     private bool collected = false;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    #region Zenject
+    [Inject]
+    private void Construct(PuzzleGameUI puzzleGameUI)
+    {
+        _puzzleGameUI = puzzleGameUI;
+    }
+    #endregion Zenject
 
     public override void Interact()
     {
@@ -16,6 +45,18 @@ public class PuzzleKey : PuzzleGameItemInteractionHandler
         {
             collected = true;
             Debug.Log($"Save Key {keyIndex}");
+            MoveToInventory();
         }
+    }
+
+    private void MoveToInventory()
+    {
+        Vector3 startPos = transform.position;
+        transform.DOJump(startPos, jumpPower, 1, jumpDuration);
+        transform.DOPunchRotation(rotationPunchVector, punchDuration, punchFreequency).OnComplete(() =>
+        {
+            _puzzleGameUI.MoveKeyToInventoryBar(spriteRenderer);
+            gameObject.SetActive(false);
+        });
     }
 }
--- Assets/Scripts/UI/Puzzle Game UI/PuzzleGameUI.cs
+++ Assets/Scripts/UI/Puzzle Game UI/PuzzleGameUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Collections;
 using TMPro;
@@ -19,8 +20,27 @@ public class PuzzleGameUI : MainCanvasPanel
     [Space]
     [SerializeField] private float bonusTimeTextChangeAlphaDurationMax = 1f;
     [SerializeField] private float bonusTimeTextChangeAlphaDurationMin = 0.01f;
+    [SerializeField] private float moveToInventoryPanelDuration = 1f;
     [Header("Panels")]
     [Space]
     [SerializeField] private KitchenMiniGameBonusTimersPanel kitchenMiniGameBonusTimersPanel;
     [SerializeField] private PanelActivationManager gameFinishedPanelActivationManager;
+    [Header("Inventory Panel")]
+    [Space]
+    [SerializeField] private Transform inventoryPanel;
+    [Header("Prefabs")]
+    [Space]
+    [SerializeField] private Image keyImagePrefab;
+
+    public Transform InventoryPanel { get => inventoryPanel; }
+
+    public void MoveKeyToInventoryBar(SpriteRenderer key)
+    {
+        Vector3 spawnPos = Camera.main.WorldToScreenPoint(new Vector3(key.transform.position.x, key.transform.position.y, 0f));
+        var keyImage = Instantiate(keyImagePrefab, spawnPos, Quaternion.identity, transform);
+        keyImage.transform.DOMove(inventoryPanel.position, moveToInventoryPanelDuration).OnComplete(() =>
+        {
+            
+        });
+    }
 }
