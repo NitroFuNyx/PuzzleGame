02aacb7fd63a1702bd52a62db37f37c1545085a4
Mykyta Hefsymanskyi <mykytahefsymanskyi@gmail.com>
2023-04-06 16:19:24 +0300

    Save load system updated

--- Assets/Scripts/Save Load System/FileDataHandler.cs
+++ Assets/Scripts/Save Load System/FileDataHandler.cs
@@ -1,28 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 public static class FileDataHandler
 {
-    //private const string PlayerDataSaveFilePath = "Assets/Save Files/PlayerSaveFile.json";
-    private const string PlayerDataSaveFilePath = "Assets/Save Files/PlayerMainDataSave.bsf";
-    //private static string playerDataSaveFilePathInBuild = $"{Application.persistentDataPath}/{fileName}";
-    private static string playerDataSaveFilePathInBuild = $"{Application.persistentDataPath}/PlayerMainDataSave.bsf";
-
-    //private static string fileName = "PlayerSaveFile.json";
+    private const string PlayerDataSaveFilePath = "Assets/Save Files/PlayerSaveFile.json";
+    private static string playerDataSaveFilePathInBuild = $"{Application.persistentDataPath}/PlayerSaveFile.json";
 
     public static GameData Read()
     {
-        GameData data = null;
+        GameData loadedData = null;
 
         if (File.Exists(GetSaveFilePath()))
         {
-            BinaryFormatter formatter = new BinaryFormatter();
+            string dataToLoad = "";
+
             FileStream stream = new FileStream(GetSaveFilePath(), FileMode.Open);
 
-            data = formatter.Deserialize(stream) as GameData;
+            StreamReader reader = new StreamReader(stream);
+
+            dataToLoad = reader.ReadToEnd();
+
+            loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+
             stream.Close();
         }
         else
@@ -30,55 +29,21 @@ public static class FileDataHandler
             Debug.Log($"No Save File {GetSaveFilePath()}");
         }
 
-        return data;
-
-
-        //GameData loadedData = null;
-
-        //if (File.Exists(GetSaveFilePath()))
-        //{
-        //    string dataToLoad = "";
-
-        //    FileStream stream = new FileStream(GetSaveFilePath(), FileMode.Open);
-
-        //    StreamReader reader = new StreamReader(stream);
-
-        //    dataToLoad = reader.ReadToEnd();
-
-        //    loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
-
-        //    stream.Close();
-        //}
-        //else
-        //{
-        //    Debug.Log($"No Save File {GetSaveFilePath()}");
-        //}
-
-        //return loadedData;
+        return loadedData;
     }
 
     public static void Write(GameData data)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
+        string dataToStore = JsonUtility.ToJson(data, true);
 
-        FileStream stream = new FileStream(GetSaveFilePath(), FileMode.Create);
-
-        //PlayerMainData playerMainData = new PlayerMainData(playerDataManager);
-
-        formatter.Serialize(stream, data);
-        stream.Close();
-
-
-        //string dataToStore = JsonUtility.ToJson(data, true);
-
-        //using (FileStream stream = new FileStream(GetSaveFilePath(), FileMode.Create))
-        //{
-        //    using (StreamWriter writer = new StreamWriter(stream))
-        //    {
-        //        writer.Write(dataToStore);
-        //    }
-        //    stream.Close();
-        //}
+        using (FileStream stream = new FileStream(GetSaveFilePath(), FileMode.Create))
+        {
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.Write(dataToStore);
+            }
+            stream.Close();
+        }
     }
 
     private static string GetSaveFilePath()
