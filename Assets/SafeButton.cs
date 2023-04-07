653dd1932cac1b75f3e1bdd14ef3e4da00d9ef46
Alexandr Meleshko <ushio159@gmail.com>
2023-04-07 16:01:50 +0200

    Added safe UI

--- Assets/SafeButton.cs
+++ Assets/SafeButton.cs
@@ -1,8 +1,22 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class SafeButton : MonoBehaviour
 {
     [SerializeField] private string safeButtonNumber;
-
+    [SerializeField] private Button button;
+    [SerializeField] private SafeButtonsHandler safeButtonsHandler;
     public string SafeButtonNumber => safeButtonNumber;
+    public event Action<string> ButtonPressed; 
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonPressed);
+    }
+
+    private  void OnButtonPressed()
+    {
+        ButtonPressed?.Invoke(safeButtonNumber);
+    }
 }
--- Assets/SafeButtonsHandler.cs
+++ Assets/SafeButtonsHandler.cs
@@ -1,8 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using Zenject;
 
 public class SafeButtonsHandler : MonoBehaviour
 {
-    
+    [SerializeField] private List<SafeButton> numButtons;
+    [SerializeField] private TextMeshProUGUI safeString;
+    private PuzzleGameUI _puzzleGameUI;
+
+    private void Start()
+    {
+        StartCoroutine(LateStart());
+    }
+    #region Zenject
+    [Inject]
+    private void Construct(PuzzleGameUI puzzleGameUI)
+    {
+        _puzzleGameUI = puzzleGameUI;
+    }
+    #endregion Zenject
+    public IEnumerator LateStart()
+    {
+        yield return new WaitForSeconds(.5f);
+        foreach (var buttons in numButtons)
+        {
+            buttons.ButtonPressed += AddNumberToSafeString;
+        }
+    }
+
+    public void AddNumberToSafeString(string Number)
+    {
+        if (safeString.text.Length < 6)
+        {
+            safeString.text += Number;
+        }
+
+        if (safeString.text.Length >= 6)
+        {
+            if (safeString.text == "631128")// Successfully written code
+            {
+                
+                StartCoroutine(ClosingSafeUi());
+            }
+            else// Wrong written code
+            {
+                StartCoroutine(DeletingText());
+
+            }
+        }
+    }
+
+    public IEnumerator ClosingSafeUi()
+    {
+        yield return new WaitForSeconds(0.75f);
+        _puzzleGameUI.ShowMainModePanel();
+
+    }
+    public IEnumerator DeletingText()
+    {
+        yield return new WaitForSeconds(0.75f);
+        safeString.text = "";
+
+    }
+
 }
